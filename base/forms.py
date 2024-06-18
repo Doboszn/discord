d18c05a07f25e46310512cf51c62aacea8dce925
bolu <humblebrown08@gmail.com>
2024-06-18 00:36:57 -0600

    logo n stuuf

--- base/forms.py
+++ base/forms.py
@@ -4,5 +4,6 @@ from .models import Room
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        #fields = '__all__'
+        #
+        fields = '__all__'
         exclude = ['host', 'participants']
