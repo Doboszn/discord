5c56e17b42c7b9bd1fec267ddff46c3757b0785a
bolu <humblebrown08@gmail.com>
2024-06-18 18:25:32 -0600

    theme correction

--- base/forms.py
+++ base/forms.py
@@ -1,10 +1,6 @@
 from django.forms import ModelForm
-from .models import Room, User
 from django.contrib.auth.forms import UserCreationForm
-
-
-
-
+from .models import Room, User
 
 class RoomForm(ModelForm):
     class Meta:
@@ -12,4 +8,9 @@ class RoomForm(ModelForm):
         fields = '__all__'
         exclude = ['host', 'participants']
 
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
         
